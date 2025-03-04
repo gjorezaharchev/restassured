2966e8a3c31152883b3de4503c12952a9911aa36
Gjore Zaharchev <Gjore.Zaharchev@seavus.com>
2025-03-04 20:29:31 +0100

    allure report plugin added

--- src/test/java/com/gjore/test/Test1.java
+++ src/test/java/com/gjore/test/Test1.java
@@ -1,13 +1,21 @@
 package com.gjore.test;
 
-import io.qameta.allure.Description;
+import io.qameta.allure.*;
+import io.qameta.allure.severity.SeverityLevel;
 import org.testng.annotations.Test;
+
+import static io.qameta.allure.severity.SeverityLevel.*;
 
 public class Test1 {
 
     @Test
-    @Description("sample test")
     public void test1(){
         System.out.println("This is my test");
+        Allure.description("This test attempts to log into the website using a login and a password. Fails if any error happens.\n\nNote that this test does not test 2-Factor Authentication.");
+        Allure.label("severity", "critical");
+        Allure.label("owner", "John Doe");
+        Allure.link("Website", "https://dev.example.com/");
+        Allure.issue("AUTH-123", "https://example.com/issues/AUTH-123");
+        Allure.tms("TMS-456", "https://example.com/tms/TMS-456");
     }
 }

f21bfa0813dbb58ac069e67a3fde0195792a0b31
Gjore Zaharchev <Gjore.Zaharchev@seavus.com>
2025-03-04 20:15:27 +0100

    allure report plugin added

--- src/test/java/com/gjore/test/Test1.java
+++ src/test/java/com/gjore/test/Test1.java
@@ -1,10 +1,12 @@
 package com.gjore.test;
 
+import io.qameta.allure.Description;
 import org.testng.annotations.Test;
 
 public class Test1 {
 
     @Test
+    @Description("sample test")
     public void test1(){
         System.out.println("This is my test");
     }

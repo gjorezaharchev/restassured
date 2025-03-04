b48e4d4f5ca3e965aee00a61ff0ab44f2a785a39
Gjore Zaharchev <Gjore.Zaharchev@seavus.com>
2025-03-04 19:06:19 +0100

    Initial commit

---
+++ src/test/java/com/gjore/test/Test1.java
@@ -0,0 +1,11 @@
+package com.gjore.test;
+
+import org.testng.annotations.Test;
+
+public class Test1 {
+
+    @Test
+    public void test1(){
+        System.out.println("This is my test");
+    }
+}
